8dc2c9dac00f024532b09bd849eae7d3ada45b21
Ravi Radadiya <raviradadiya30@gmail.com>
2022-01-18 18:59:06 -0500

    Add files via upload

---
+++ lab1_raviradadiya_0836175_ios/ViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  lab1_raviradadiya_0836175_ios
+//
+//  Created by Ravi Radadiya on 2022-01-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var activePlayer = 1
+    var gameState = [0,0,0,0,0,0,0,0,0]
+    var winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    var gameIsActive = true
+    
+    @IBAction func Buttons (_ sender: UIButton)
+    {
+        if(gameState[sender.tag-1] == 0 && gameIsActive == true )
+        {
+            gameState[sender.tag-1] = activePlayer
+            if(activePlayer == 1)
+            {
+                sender.setImage(UIImage(named: "cross.png"), for: UIControl.State())
+                activePlayer = 2
+            }
+            else
+            {
+                sender.setImage(UIImage(named: "circle.png"), for: UIControl.State())
+                activePlayer = 1
+            }
+        }
+        
+        for combination in winningCombinations {
+            if gameState[combination[0]] != 0 &&
+                gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]
+            {
+                gameIsActive = false
+                
+                if gameState[combination[0]] == 1
+                {
+                    label.text = "CROSS HAS WON!"
+                    
+                }
+                else{
+                    label.text = "NOUGHT HAS WON!"
+                }
+                
+                PlayAgainButton.isHidden = false
+                label.isHidden = false
+            }
+        }
+        
+        gameIsActive = false
+        
+        for i in gameState
+        {
+            if i == 0
+            {
+                gameIsActive = true
+                break
+            }
+        }
+        
+        if gameIsActive == false
+        {
+            label.text = "Game WAS DRAW"
+            label.isHidden = false
+            PlayAgainButton.isHidden = false
+        }
+        
+    }
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var PlayAgainButton: UIButton!
+    
+    @IBAction func PlayAgain(_ sender: UIButton)
+    {
+        gameState = [0,0,0,0,0,0,0,0,0]
+        gameIsActive = true
+        activePlayer = 1
+        
+        PlayAgainButton.isHidden = true
+        label.isHidden = true
+        
+        for i in 1...9
+        {
+            let button = view.viewWithTag(i) as! UIButton
+            button.setImage(nil, for: UIControl.State())
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ lab1_raviradadiya_0836175_iosUITests/lab1_raviradadiya_0836175_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  lab1_raviradadiya_0836175_iosUITestsLaunchTests.swift
+//  lab1_raviradadiya_0836175_iosUITests
+//
+//  Created by Ravi Radadiya on 2022-01-18.
+//
+
+import XCTest
+
+class lab1_raviradadiya_0836175_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
